69cc779d7cf38d1568a50a49e2272d511fc93453
Your Name <you@example.com>
2021-10-25 21:53:12 +0530

    providers

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shop App',
+      debugShowCheckedModeBanner: false,
       home: Productoverview(),
-      
-    
-
     );
-
-
-
   }
 }
-
